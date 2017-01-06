d24fb6b1cfd89842ff22f5717955b1c80bfe0716
Jake <jecobwesterfield@gmail.com>
2017-01-05 20:56:49 -0800

    iterating_arrays_js
    iterating_arrays_js

---
+++ untitled folder/iterating.js
@@ -0,0 +1,23 @@
+(function(){
+    "use strict";
+
+    // TODO: Create an array of 4 people's names using literal array notation, in a variable called 'names'.
+    var names = ['Jake', 'Mike', 'Paul', 'Sean', 'Alexa'];
+
+    // TODO: Create a log statement that will log the number of elements in the names array.
+    console.log(names.length);
+
+    // TODO: Create log statements that will print each of the names array elements individually.
+    
+ //Fifth Name
+    console.log( 'The Fifth name is: ' + "" + names[4]);
+//Fourth Name
+	console.log( 'The Fourth name is: ' + "" + names[3]);
+//Third Name
+	console.log(' The Third name is: ' + "" + names[2]);
+//Second Name
+	console.log(' The Second name is: ' + "" + names[1]);
+//First Name
+	console.log(' The First name is: ' + "" + names[0]);
+
+})();
